c616efd92bf7aa8438ee492e4cff624bab0d748e
caberiosmailan <97483079+caberiosmailan@users.noreply.github.com>
2022-01-13 03:09:00 +0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,73 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "How many layers in the TCP/IP protocol layers?",
+    answer: "4",
+    options: [
+      "7",
+      "5",
+      "4",
+      "11"
+    ]
+  },
+    {
+    numb: 2,
+    question: "'Linux' is an example of?",
+    answer: "Operating System",
+    options: [
+      "Software",
+      "Application",
+      "Browser",
+      "Operating System"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Number of bit used by the IPv6??",
+    answer: "128 bit",
+    options: [
+      "32 bit",
+      "64 bit",
+      "128 bit",
+      "256 bit"
+    ]
+  },
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
